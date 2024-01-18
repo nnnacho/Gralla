360f7c65266b3729c985ad3f4dbf09c11848cb6c
Ignacio Armijo Mansilla <incarmijo@gmail.com>
2024-01-18 00:03:32 -0300

    Save Record of Mic

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const $button = document.querySelector('button')
+const $button = document.getElementById('btn-screen')
 
 $button.addEventListener('click', async () => {
     
@@ -24,8 +24,69 @@ $button.addEventListener('click', async () => {
   })
 })
 
-navigator.mediaDevices.getUserMedia({ audio: true }).then((stream ) => {
-  console.log(stream)
-  let audio = document.getElementById('audio')
-  audio.srcObject = stream
-}).catch((err) => console.log(err))
+
+
+
+
+// Referencias DOM
+const startBtn = document.getElementById('start-record');
+const stopBtn = document.getElementById('stop-record');
+
+// Banderas
+let isRecording = false;
+let chunks = []; 
+
+
+// Habilitar/deshabilitar botones
+const toggleButtons = () => {
+  startBtn.disabled = isRecording;
+  stopBtn.disabled = !isRecording;
+}
+
+// Al hacer click en start
+startBtn.addEventListener('click', async () => {
+  isRecording = true;
+  toggleButtons();
+
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+  mediaStream = stream;
+  const mediaRecorder = new MediaRecorder(stream);
+
+  mediaRecorder.addEventListener('dataavailable', event => {
+    chunks.push(event.data);
+  });
+
+  mediaRecorder.start();
+  stopBtn.addEventListener('click', () => {
+
+    isRecording = false;
+    toggleButtons();
+  
+    mediaRecorder.addEventListener('stop', () => {
+        
+      // Código para descargar    
+      const audioBlob = new Blob(chunks, { type: 'audio/mp3' });
+      const audioUrl = URL.createObjectURL(audioBlob);
+      const link = document.createElement('a');
+      link.href = audioUrl;
+      link.download = 'recording.mp3';
+      document.body.appendChild(link);
+      link.click();
+      mediaStream.getTracks().forEach(track => track.stop());
+      
+      // Limpiar chunks
+      chunks = [];
+  
+    });
+  
+    mediaRecorder.stop();
+  
+  });
+});
+
+// Al hacer click en stop  
+
+
+// Iniciar con botones deshabilitados
+toggleButtons();
